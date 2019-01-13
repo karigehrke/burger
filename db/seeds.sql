5c00fdd836e3177262553ae6ebeed87624765e28
Vesper <no-reply>
2019-01-13 18:01:19 -0500

    working code

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krabby Patty', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krusty Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Paunch Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('White Castle', false);
