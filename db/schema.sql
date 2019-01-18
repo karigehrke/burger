d343414c8548a7ed752ba692d165ce5207086126
Vesper <no-reply>
2019-01-17 22:23:24 -0500

    heroku compatible

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-id int NOT NULL AUTO_INCREMENT,
+id INT AUTO_INCREMENT NOT NULL,
 burger_name VARCHAR(100) NOT NULL,
 devoured BOOLEAN DEFAULT false,
+createdAt TIMESTAMP NOT NULL,
 PRIMARY KEY (id)
 );
